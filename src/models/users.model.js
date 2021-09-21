4a88b44bcc05b5c98736477a3c08cae116a3caaf
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2021-09-21 19:25:06 -0300

    Update user model

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -10,11 +10,9 @@ module.exports = function User(app) {
       address: { type: String, required: true, index: true, unique: true },
       name: { type: String },
       email: { type: String },
-      giverId: { type: Schema.Types.Long }, // we can use Long here b/c lp only stores adminId in pledges as uint64
-      commitTime: { type: Number },
-      avatar: { type: String },
       linkedin: { type: String },
       url: { type: String },
+      infoCid: { type: String },
     },
     {
       timestamps: true,
