5e044d456a385b922348138b60097585e0339bf0
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2021-10-31 10:26:28 -0300

    Allow specify database url with USERS_MONGO_DB

--- config/config.js
+++ config/config.js
@@ -5,5 +5,5 @@ const configPath = path.resolve(__dirname, `./.env`)
 dotenv.config({ path: configPath });
 
 module.exports = {
-  mongodb: process.env.MONGO_DB,
+  mongodb: process.env.USERS_MONGO_DB || process.env.MONGO_DB,
 };
